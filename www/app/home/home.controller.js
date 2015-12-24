76e801a72572e2060fb960f7999f47814aa4d903
cdiaz19 <cdiaz@pernix-solutions.com>
2015-12-24 10:21:56 -0600

    add ionic project

---
+++ www/app/home/home.controller.js
@@ -0,0 +1,21 @@
+(function() {
+  'use strict';
+ 
+  angular
+    .module('budgetrentacar.home')
+    .controller('HomeController', HomeController);
+ 
+  HomeController.$inject = [];
+ 
+  /* @ngInject */
+    function HomeController() {
+    var vm = this;
+ 
+    activate();
+ 
+    ////////////////
+ 
+    function activate() {
+    }
+  }
+})(); 
