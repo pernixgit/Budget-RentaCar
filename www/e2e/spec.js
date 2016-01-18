fb952e021955f3da73d7b70a04a9adbb5dac8bfc
Oscar Corella <oscorella@gmail.com>
2016-01-18 10:59:09 -0600

    added tests

---
+++ www/e2e/spec.js
@@ -0,0 +1,11 @@
+
+describe('Budget Rent a Car', function() {
+  it('should go to carInterior page', function() {
+    browser.get('http://localhost:8100/#/carExterior');
+    element(by.css(".arrow")).click();
+    expect(browser.getCurrentUrl()).toEqual("http://localhost:8100/#/carInterior");
+    browser.get('http://localhost:8100/#/carInterior');
+    element(by.css(".arrow")).click();
+    expect(browser.getCurrentUrl()).toEqual("http://localhost:8100/#/trunk");
+  });
+});
