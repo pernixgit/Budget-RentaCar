2944d531ab33b34bf19acd59c5c477022c3ab519
Luis Grases <luisgrases@gmail.com>
2016-01-20 14:05:32 -0600

    Create basic structure for revision model

---
+++ www/app/models/revision.service.js
@@ -0,0 +1,19 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('budgetrentacar.models')
+    .service('Revision', Revision);
+
+  Revision.$inject = [];
+
+  /* @ngInject */
+  function Revision() {
+    var _model = {
+
+    };
+    return _model;
+
+  }
+
+})();
