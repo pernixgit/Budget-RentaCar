c7886b0da94b59470a81f6a444b08f7e9d788f69
Luis Grases <luisgrases@gmail.com>
2016-01-20 14:26:38 -0600

    Add functionality for pushing data

--- www/app/models/revision.service.js
+++ www/app/models/revision.service.js
@@ -5,19 +5,27 @@
     .module('budgetrentacar.models')
     .service('Revision', Revision);
 
-  Revision.$inject = [];
+  Revision.$inject = ['$firebaseObject'];
 
   /* @ngInject */
-  function Revision() {
+  function Revision($firebaseObject) {
     var _model = {
       data: {
         observations: null,
         revisions :null
-      }
+      },
+      pushData: pushData
 
     };
     return _model;
 
+
+    function pushData(data) {
+      var ref = new Firebase('https://budget-test.firebaseio.com');
+      ref.set(_model.data);
+    };
+
   }
+
 
 })();
