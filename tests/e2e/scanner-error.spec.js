ca850942abcf31c3690056ad8ef6af203556e986
Oscar Corella <oscorella@gmail.com>
2016-01-25 11:07:01 -0600

    scanner-errore2e test

--- tests/e2e/scanner-error.spec.js
+++ tests/e2e/scanner-error.spec.js
@@ -1,7 +1,9 @@
 describe('Budget Rent a Car', function() {
 	it('should go to scanner page', function() {
-		browser.get(''+'/#/scanner-error');
+		browser.get(browser.baseUrl +'scanner-error');
+		 browser.sleep(3000); 
 		element(by.css(".button.button-positive")).click();
 		expect(browser.getCurrentUrl()).toEqual("http://localhost:8100/#/scanner");
 	});
 });
+
