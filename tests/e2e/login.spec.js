5b20e8145070e098c9226fd74d63f23a51b547f6
Oscar Corella <oscorella@gmail.com>
2016-01-25 08:36:46 -0600

    BeforeEach() & declared variables outside

--- tests/e2e/login.spec.js
+++ tests/e2e/login.spec.js
@@ -1,9 +1,13 @@
 describe('Budget Rent a Car', function() {
+  var username = element(by.model('LoginController.username'));
+  var password = element(by.model('LoginController.password'));
+  beforeEach(function() {
+  	  browser.get('');	
+  	  browser.sleep(2000); 
+  });
+
   it('should go to carview page with ocorella credentials', function() {
-    browser.get('');	
-    var username = element(by.model('LoginController.username'));
     username.sendKeys('ocorella');
-    var password = element(by.model('LoginController.password'));
     password.sendKeys('ocorella');
     expect(element(by.css('.button-primary')).isPresent()).toBe(true);
     element(by.css(".button-primary")).click();
@@ -12,10 +16,7 @@ describe('Budget Rent a Car', function() {
   });
 
   it('should stay in login page when using wrong crendetials and show error popup', function() {
-    browser.get('');	
-    var username = element(by.model('LoginController.username'));
     username.sendKeys('SIRIASNOTIENECREDENCIALES');
-    var password = element(by.model('LoginController.password'));
     password.sendKeys('abcdefasdasd');
     expect(element(by.css('.button-primary')).isPresent()).toBe(true);
     element(by.css(".button-primary")).click();
@@ -25,10 +26,7 @@ describe('Budget Rent a Car', function() {
   });
 
   it('should stay in login page and show popup when clicking the register <a> element', function() {
-    browser.get('');	
-    var username = element(by.model('LoginController.username'));
     username.sendKeys('SIRIASNOTIENECREDENCIALES');
-    var password = element(by.model('LoginController.password'));
     password.sendKeys('abcdefasdasd');
     expect(element(by.css('.link-button')).isPresent()).toBe(true);
     element(by.css(".link-button")).click();
