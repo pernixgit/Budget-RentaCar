67e09d3cb4bbaf392d489e0b48bde6b69c99332f
Oscar Corella <oscorella@gmail.com>
2016-01-25 11:27:05 -0600

    used browser.baseUrl

--- tests/e2e/carInt.spec.js
+++ tests/e2e/carInt.spec.js
@@ -1,6 +1,6 @@
 describe('Budget Rent a Car', function() {
   it('should go to trunk page', function() {
-    browser.get(''+'/#/carInterior');
+	browser.get(browser.baseUrl +'carInterior');
     element(by.css(".arrow")).click();
     expect(browser.getCurrentUrl()).toEqual("http://localhost:8100/#/trunk");
   });
