5ce9fe10ad7558f9ed6da458258e54db5b38b192
Oscar Corella <oscorella@gmail.com>
2016-01-26 14:49:31 -0600

    tests inside corresponding view folder

--- protractor.conf.js
+++ protractor.conf.js
@@ -2,5 +2,6 @@ exports.config = {
   framework: 'jasmine',
   seleniumAddress: 'http://localhost:4444/wd/hub',
   baseUrl: 'http://localhost:8100/#/',
-  specs: ['tests/e2e/*.spec.js']
+  specs: ['tests/e2e/**/*.spec.js']
 }
+
