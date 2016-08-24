3ca81921d2d83124fa1aa4e9b3e1cd2775a673e4
pfernandez01 <jfernandez@pernix-solutions.com>
2016-08-24 14:59:00 -0600

    Test e2e , specs

--- tests/specs/carInfo/carInfo.controller.spec.js
+++ tests/specs/carInfo/carInfo.controller.spec.js
@@ -1,3 +1,30 @@
-/**
- * Created by jxu on 5/24/16.
- */
+describe('carInfo Controller', function() {
+  var controller, CarInfoFirebaseService, $state, RevisionService, LastRevisionService, $ionicNavBarDelegate;
+
+  beforeEach(module('ui.router'));
+  beforeEach()
+  beforeEach(module('budgetrentacar.carInfo'));
+  beforeEach(inject(function($controller, $state)
+
+  LastRevisionService = {
+    revision: {damages:[]},
+    fetchRevisionData: function(){}
+  };
+
+  CarInfoFirebaseService = {
+    carInfo: {traction_type: '4x4'}
+  };
+
+  controller = $controller('CarInfoCtrl',  {
+    '$state' : $scope,
+    'RevisionService' : RevisionService,
+    '$ionicNavBarDelegate' : $ionicNavBarDelegate,
+    'LastRevisionService' : LastRevisionService,
+    'CarInfoFirebaseService' : CarInfoFirebaseService
+  });
+
+  it('Should create controller', function() {
+    expect(controller).not.toBeUndefined();
+  });
+
+});
---
+++ tests/specs/carInfo/carInfo.services.spec.js
@@ -0,0 +1,13 @@
+describe ('CarInfoFirebaseService', function(){
+
+  var RevisionService;
+
+  beforeEach(module('budgetrentacar.services'));
+  beforeEach(inject(function(_CarInfoFirebaseService_) {
+    CarInfoFirebaseService = _CarInfoFirebaseService_;
+  }));
+
+  it('Should set the username to carInfo object', function() {
+
+  });
+});
