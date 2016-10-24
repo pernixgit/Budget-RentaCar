aea89f053ba4e2e854927a46df9d4e640d3c41ea
Johnny Xu <johnnyxl26@gmail.com>
2016-10-24 09:16:12 -0600

    Refactor scannner

--- www/app/scanner/scanner.controller.js
+++ www/app/scanner/scanner.controller.js
@@ -6,18 +6,29 @@
   .controller('ScannerCtrl', ScannerCtrl);
 
   /* @ngInject */
-
   function ScannerCtrl($state, $cordovaBarcodeScanner, ScannerService) {
 
-    $cordovaBarcodeScanner.scan()
-      .then(
-        function(code) {
-          ScannerService.setCode(code.text);
-          $state.go('carInfo');
-        },
-        function(error) {
-          alert('Error, no se pudo leer el código');
-          $state.go('scanner-error');
-        });
+    activate();
+
+    function activate() {
+      scanCode();
+    }
+
+    function scanCode() {
+      $cordovaBarcodeScanner.scan()
+        .then(handleScanSuccess)
+        .catch(handleScanError);
+    }
+
+    function handleScanSuccess(code) {
+      ScannerService.setCode(code.text);
+      $state.go('carInfo');
+    }
+
+    function handleScanError() {
+      alert('Error, no se pudo leer el código');
+      $state.go('scanner-error');
+    }
+
   }
 })();
